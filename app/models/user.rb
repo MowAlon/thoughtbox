e320fb7b3c120eff1318b955022989dd5510d8e6
Alon Waisman <mowalon@yahoo.com>
2016-01-11 10:03:41 -0700

    Add User
    Added user model (name, password_digest)
    Added sessions controller
    Added bcrypt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :name, presence: true, length: { maximum: 32 }
+  validates :password, presence: true, length: { minimum: 6 }
+
+  has_secure_password
 end
