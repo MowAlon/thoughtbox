af6faa0c75bd5243036e44e1d20d62ea7d58136a
Alon Waisman <mowalon@yahoo.com>
2016-01-11 11:20:44 -0700

    Test user validation
    Added tests for presence, uniqueness, and length of user's name
    Added tests for presence and length of password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true, length: { maximum: 32 }
+  validates :name, presence: true, uniqueness: true, length: { maximum: 32 }
   validates :password, presence: true, length: { minimum: 6 }
 
   has_secure_password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.create(name: 'Example',
+                      password: 'password', password_confirmation: 'password')
+  end
+
+  it "should be valid" do
+    expect(@user).to be_valid
+  end
+
+  context "name" do
+    it "should not be blank" do
+      @user.name = ""
+      expect(@user).to be_invalid
+    end
+
+    it "should be 32 characters or less" do
+      @user.name = "12345678901234567890123456789012"
+      expect(@user).to be_valid
+      @user.name = "123456789012345678901234567890123"
+      expect(@user).to be_invalid
+    end
+
+    it "should not allow two users with the same name" do
+      user_count = User.count
+      user2 = User.create(name: 'Example',
+      password: 'password', password_confirmation: 'password')
+
+      expect(User.count).to eq(user_count)
+      expect(user2.errors.full_messages).to include("Name has already been taken")
+
+    end
+  end
+
+  context "password" do
+    it "should not be blank" do
+      @user.update_attributes(password: "", password_confirmation: "")
+      expect(@user).to be_invalid
+    end
+
+    it "should be at least six characters" do
+      @user.update_attributes(password: "test", password_confirmation: "test")
+      expect(@user).to be_invalid
+      expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+    end
+  end
+
 end
