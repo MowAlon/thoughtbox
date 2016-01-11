d848ea62a7efb2db015a225d628a4a5369a3e5f0
Alon Waisman <mowalon@yahoo.com>
2016-01-11 10:16:14 -0700

    Logging in
    Added routes for logging in and force to login page if the user isn't logged in. Login page is only a stand-in. No form for logging in exists yet.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+  skip_before_action :verify_user, only: [:new, :create]
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  get "logout", to: "sessions#destroy"
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
